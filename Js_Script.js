fb8a15f514374a4f84be152c1e1a6062e73b9e6b
Dalia Mohamed <133280610+daliaelmnofy@users.noreply.github.com>
2024-01-09 15:27:35 +0300

    Add files via upload

---
+++ Js_Script.js
@@ -0,0 +1,60 @@
+
+    const logoutLink = document.getElementById('logoutLink');
+
+    logoutLink.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        logoutcust();
+    });
+
+    function logoutcust() {
+        localStorage.removeItem("username");
+        localStorage.removeItem("useremail");
+        localStorage.removeItem("userpassword");
+        window.location.href = "SignUp&Login/LOGIN.html";
+    }
+
+
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    });
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
+}
+// Fetch data from localStorage and display products
+
+function showDataInUI() {
+    const productsSection = document.getElementById('product2');
+    console.log(productsSection)
+    const storedData = localStorage.getItem('product');
+    const dataPro = JSON.parse(storedData) || [];
+
+    let productsHTML = '';
+
+    dataPro.forEach((product, _index) => {
+        productsHTML += `
+            <div class="pro">
+                <img src="${product.image}" alt="${product.title}">
+                <div class="des">
+                    <span>${product.title}</span>
+                    <h5>${product.description}</h5>
+                    <h4>${product.total}$</h4>
+                    <a class="add-cart"><i class="fal fa-shopping-cart cart"></i></a>
+                </div>
+            </div>`;
+    });
+
+    productsSection.innerHTML = productsHTML;
+}
+
+showDataInUI();
